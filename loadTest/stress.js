45cfbdb9aa27bb6bd46ee069a1b15cb892b8ac50
FKgk <rhkd865@gmail.com>
2023-02-25 14:21:50 +0900

    refac: 불필요 설정 제거

--- loadTest/stress.js
+++ loadTest/stress.js
@@ -3,22 +3,19 @@ import {getAuthHeaders} from './config.js';
 import {checkLine, checkStation, checkPath} from './functions.js';
 
 export const options = {
-    scenarios: {
-        stress: {
-            executor: "ramping-arrival-rate",
-            timeUnit: "1s",
-            stages: [
-                { duration: "2m", target: 100 }, // below normal load
-                { duration: "5m", target: 200 },
-                { duration: "2m", target: 300 }, // normal load
-                { duration: "5m", target: 300 },
-                { duration: "2m", target: 400 }, // around the breaking point
-                { duration: "5m", target: 400 },
-                { duration: "2m", target: 500 }, // beyond the breaking point
-                { duration: "5m", target: 500 },
-                { duration: "10m", target: 0 }, // scale down. Recovery stage.
-            ],
-        },
+    stages: [
+        { duration: "2m", target: 100 }, // below normal load
+        { duration: "5m", target: 200 },
+        { duration: "2m", target: 300 }, // normal load
+        { duration: "5m", target: 300 },
+        { duration: "2m", target: 400 }, // around the breaking point
+        { duration: "5m", target: 400 },
+        { duration: "2m", target: 500 }, // beyond the breaking point
+        { duration: "5m", target: 500 },
+        { duration: "2m", target: 0 }, // scale down. Recovery stage.
+    ],
+    thresholds: {
+        'http_req_duration': ['p(99)<1500'], // 99% of requests must complete below 1.5s
     },
 };
 export default () => {
